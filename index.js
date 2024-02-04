564b42770a6ede76874bd08348ef508620072243
Palak_k <116274445+palakk4563@users.noreply.github.com>
2024-02-04 21:47:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+// -------------about me ----------
+var tablinks = document.getElementsByClassName('tab-links');
+var tabcontents = document.getElementsByClassName('tab-contents');
+
+function openTab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    for(tabcontent of tabcontents )
+    tabcontent.classList.remove("active-tab");
+    }
+    
+    event.currentTarget.classList.add('active-link');
+   document.getElementById(tabname).classList.add("active-tab");
+}
+// -----------------------------------------------------
+
+// ------------------Contact-------------
+const scriptURL =
+        "https://script.google.com/macros/s/AKfycbyPfpFgIUWE9-E99XJOJD8Yw1mavnK2U8CZFNi3-HqK-32_WWPbF6lLcfrsAS5geZML/exec";
+      const form = document.forms["submit-to-google-sheet"];
+    const msg = document.getElementById("msg");
+
+      form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        fetch(scriptURL, { method: "POST", body: new FormData(form) })
+          .then((response) => {
+            msg.innerHTML="Message Sent Successfully...."
+            setTimeout(function(){
+                msg.innerHTML = ""
+            },5000)
+            form.reset()
+          })
+          .catch((error) => console.error("Error!", error.message));
+      });
+
+// -----------------------Sticky Nav-----------------------
+
+window.addEventListener("scroll",function(){
+    var header = document.querySelector("nav");
+    navbar.classList.toggle("sticky",window.scrollY > 0);
+})
